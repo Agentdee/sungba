39363a62fad3dd181066e7277210ea6c123ebe4a
jnke <you@kjdnkwj.com>
2022-09-14 03:24:51 +0200

    First commit Message

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const port = process.env.PORT || 9000;
     visitorIp = null,
     db = null;
 
-mongoose.connect("mongodb+srv://ever:kzwFwXCHStQa3YG@cluster0.w79ga.mongodb.net/myFirstDatabase?retryWrites=true&w=
-majority", {
+mongoose.connect("mongodb+srv://ever:kzwFwXCHStQa3YG@cluster0.w79ga.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
